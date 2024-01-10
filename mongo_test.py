ec9cb4f7d21e08b06d8aeb45236aff5d9238794c
hyun0547 <hyun0547@gmail.com>
2024-01-10 09:06:24 +0900

    mongo insert test complete

--- mongo_test.py
+++ mongo_test.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import yaml
+import json
 
 with open('config.yaml') as f:
     CONFIG = yaml.load(f, Loader=yaml.FullLoader)
@@ -7,7 +8,18 @@ with open('config.yaml') as f:
 MONGO_CONFIG = CONFIG['database']['mongo']
 
 client = MongoClient(host=MONGO_CONFIG['host'], port=MONGO_CONFIG['port'], username=MONGO_CONFIG['username'], password=MONGO_CONFIG['password'])
+movies = client.MovieHAM.movies
 
-db = client.MovieHAM
+movies.drop()
 
-print(db)
+#sample
+with open('./sample_movie.json', 'r') as f:
+    movie = json.load(f)
+
+# id => _id rename key
+movie = {"_id" if k == "id" else k:v for k,v in movie.items()}
+movies.insert_one(movie)
+
+print(movies.find_one())
+
+
