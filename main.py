719893acbc5a05c60d5c373ad0b77a6dc9c5f00e
hyun0547 <hyun0547@gmail.com>
2024-01-23 09:20:13 +0900

    make now playing movie insert process

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import api_tmdb
 import mongo_MovieHAM 
+
+
        
-movies = api_tmdb.get_tmdb_now_playing_movies()
+def now_playing_movies(page):
+    
+    movies = api_tmdb.get_tmdb_now_playing_movies()
+    try:
+        
+        movieList = []    
+        for movie in movies:
+            movieDetail = api_tmdb.rename_field('id', '_id', api_tmdb.get_tmdb_movie_detail(movie['id']))
+            movieDetail['cast'] = people_init(movie['id'])
+            movieList.append(movieDetail)
+    
+        if(movies['total_pages'] > page):
+            movieList += now_playing_movies(page+1)
 
-mongo_MovieHAM.mongo_insert_many(movies)
+        return movieList
+        
+    except Exception as ex:
+        print(ex)        
+        
+        
+        
+def people_init(movieId):
+    people = api_tmdb.get_tmdb_movie_people(movieId)
+    for person in people:
+        try:
+            mongo_MovieHAM.mongo_insert_many_people(api_tmdb.get_tmdb_person('query', person['original_name']))
+        except Exception as ex:
+            print(ex)
+        
+    return people
 
+
+
+mongo_MovieHAM.mongo_insert_many_movies(api_tmdb.get_tmdb_now_playing_movies(1))
